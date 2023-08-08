9883d440c1838bd8821de071293e03b5a32c2b8a
Alban King <alban.king@zervant.com>
2023-08-08 16:42:15 +0300

    remove quotes from args. file as positional arg

--- check_message.py
+++ check_message.py
@@ -12,16 +12,21 @@ def get_n_commits_in_branch(repo, default_branch=None) -> int:
 
 def main(argv=None) -> int:
     parser = argparse.ArgumentParser("Asserts the first commit to a branch matches a regex")
-    parser.add_argument("--commit-msg-filename", help="name of the input to check against")
+    parser.add_argument("COMMIT_EDITMSG", help="name of the input to check against")
     parser.add_argument("-p", "--pattern", type=str, help="regex pattern that the commit-msg-file must match")
     parser.add_argument("-n", "--first-n", type=int, default=1, help="how many commits on the branch should match")
     parser.add_argument("-r", "--repo-path", default=".")
+    parser.add_argument("--debug", action="store_true")
 
     args, _ = parser.parse_known_args(argv)
 
+    if args.debug:
+        print("args:", args)
+        print("unparsed args:", _)
+
     repo = Repo(args.repo_path)
 
-    commit_msg = Path(args.commit_msg_filename).read_text()
+    commit_msg = Path(args.COMMIT_EDITMSG).read_text()
     # this is a commit-msg hook, so it happens before the first commit on the branch
     if get_n_commits_in_branch(repo) < args.first_n and not re.match(args.pattern, commit_msg):
         print(f"commit message = {commit_msg!r} does not match the pattern {args.pattern}", file=sys.stderr)
